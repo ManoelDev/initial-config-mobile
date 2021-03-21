1d30a541b947a5cbdb44a9015600718c992c4152
ManoelDev <maguserv@gmail.com>
2021-03-21 04:43:05 -0300

    intial config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <Text>Hello world</Text>;
+};
+
+export default App;
